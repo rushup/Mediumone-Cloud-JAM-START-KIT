af6bb4af35a4e9f0a63eaefe99a1028673bd6943
Matteo Fumagalli <matteofumagalli1275@gmail.com>
2018-05-10 10:22:44 +0200

    del support

--- Common/App/Src/platform_usb.c
+++ Common/App/Src/platform_usb.c
@@ -7,13 +7,18 @@ int usb_data_buffer_index = 0;
 
 uint16_t new_usb_data_cb(char* data, uint16_t len)
 {
+
 #if REDIRECT_WIFI_TO_USB == 1
   uart_send(&uart_wifi, data, len);
 #else
   for(int i=0; i < len; i++)
   {
+//    static char bb[10];
+//    snprintf(bb, 10, "%d ", data[i]);
+//    uart_send(&uart_usb, bb, strlen(bb));
+//    uart_send(&uart_usb, "\r\n", 2);
     
-    if(data[i] != '\b')
+    if(data[i] != '\b' && data[i] != 127) //127 = DEL
     {
       if(usb_data_buffer_index < (USB_DATA_BUFFER_SIZE - 2))
         usb_data_buffer[usb_data_buffer_index++] = data[i];
@@ -24,10 +29,12 @@ uint16_t new_usb_data_cb(char* data, uint16_t len)
       usb_data_buffer_index--;
     }
     
-    if(data[i] != '\r' && data[i] != '\b')
+    if(data[i] != '\r' && data[i] != '\b' && data[i] != 127) //127 = DEL
       uart_send(&uart_usb, &data[i], 1);
-    if(data[i] == '\b')
-      uart_send(&uart_usb, "\b \b", 3);
+    
+    /* DEL acts as backspace on screen (linux) */
+    if(data[i] == '\b' || data[i] == 127) //127 = DEL
+      uart_send(&uart_usb, "\033[D \033[D", strlen("\033[D \033[D"));
   }
   
 #endif
